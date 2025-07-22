cf749f25d55a4341dd2c103bfbfe05ebabb7d7a9
saucepoint <saucepoint@protonmail.com>
2025-07-22 13:35:10 -0400

    move to test base

--- test/Firepit.t.sol
+++ test/Firepit.t.sol
@@ -1,41 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.29;
 
-import {Test} from "forge-std/Test.sol";
+import {PhoenixTestBase} from "./utils/PhoenixTestBase.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import {Currency} from "v4-core/types/Currency.sol";
 
 import {Firepit} from "../src/Firepit.sol";
 import {AssetSink} from "../src/AssetSink.sol";
 
-contract FirepitTest is Test {
-  AssetSink assetSink;
-  address owner;
-  address alice;
-  MockERC20 resource;
-  MockERC20 mockToken;
-
-  Firepit public firepit;
-
-  uint256 public constant INITIAL_TOKEN_AMOUNT = 1000e18;
-
-  function setUp() public {
-    resource = new MockERC20("BurnableResource", "BNR", 18);
-    mockToken = new MockERC20("MockToken", "MTK", 18);
-    owner = makeAddr("owner");
-    alice = makeAddr("alice");
-    assetSink = new AssetSink(owner);
-
-    firepit = new Firepit(address(resource), INITIAL_TOKEN_AMOUNT, address(assetSink));
+contract FirepitTest is PhoenixTestBase {
+  function setUp() public override {
+    super.setUp();
 
     vm.prank(owner);
     assetSink.setReleaser(address(firepit));
-
-    // Mint tokens and send to AssetSink
-    mockToken.mint(address(assetSink), INITIAL_TOKEN_AMOUNT);
-
-    // Mint burnable resource to a user.
-    resource.mint(alice, INITIAL_TOKEN_AMOUNT);
   }
 
   function test_torch_success() public {
@@ -56,5 +34,23 @@ contract FirepitTest is Test {
     assertEq(resource.balanceOf(alice), 0);
     assertEq(resource.balanceOf(address(firepit)), 0);
     assertEq(resource.balanceOf(address(0)), INITIAL_TOKEN_AMOUNT);
+  }
+
+  function test_fuzz_revert_torch_insufficient_balance(uint256 amount) public {
+    // Assets to collect.
+    Currency[] memory assets = new Currency[](1);
+    assets[0] = Currency.wrap(address(mockToken));
+
+    amount = bound(amount, 1, resource.balanceOf(alice));
+    // alice spends some of her resources
+    vm.prank(alice);
+    resource.transfer(address(0), amount);
+
+    assertLt(resource.balanceOf(alice), firepit.THRESHOLD());
+
+    vm.startPrank(alice);
+    resource.approve(address(firepit), type(uint256).max);
+    vm.expectRevert(address(resource)); // reverts on token insufficient allowance
+    firepit.torch(assets, alice);
   }
 }
