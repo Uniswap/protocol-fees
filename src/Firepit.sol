3a1994c932bfafef5c98ea9b6e8be2e9d666a97c
saucepoint <saucepoint@protonmail.com>
2025-07-23 16:05:34 -0400

    safeTransferFrom

--- src/Firepit.sol
+++ src/Firepit.sol
@@ -2,17 +2,19 @@
 pragma solidity ^0.8.29;
 
 import {Currency} from "v4-core/types/Currency.sol";
-import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {SafeTransferLib, ERC20} from "solmate/utils/SafeTransferLib.sol";
 import {AssetSink} from "./AssetSink.sol";
 import {Nonce} from "./base/Nonce.sol";
 
 contract Firepit is Nonce {
-  IERC20 public immutable RESOURCE;
+  using SafeTransferLib for ERC20;
+
+  ERC20 public immutable RESOURCE;
   uint256 public immutable THRESHOLD;
   AssetSink public immutable ASSET_SINK;
 
   constructor(address _resource, uint256 _threshold, address _assetSink) {
-    RESOURCE = IERC20(_resource);
+    RESOURCE = ERC20(_resource);
     THRESHOLD = _threshold;
     ASSET_SINK = AssetSink(_assetSink);
   }
@@ -21,7 +23,7 @@ contract Firepit is Nonce {
     external
     handleNonce(_nonce)
   {
-    RESOURCE.transferFrom(msg.sender, address(0), THRESHOLD);
+    RESOURCE.safeTransferFrom(msg.sender, address(0), THRESHOLD);
 
     for (uint256 i = 0; i < assets.length; i++) {
       ASSET_SINK.release(assets[i], recipient);
