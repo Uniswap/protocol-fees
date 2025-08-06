19e3c5a55ee46a5a430879fb08bb92b1684d041b
saucepoint <saucepoint@protonmail.com>
2025-08-05 20:03:00 -0400

    remove soft nonce

--- src/base/Nonce.sol
+++ src/base/Nonce.sol
@@ -14,33 +14,3 @@ abstract contract Nonce {
     _;
   }
 }
-
-abstract contract SoftNonce is Nonce {
-  uint256 public lastSeenTimestamp;
-
-  uint256 public constant EXPIRATION = 30 minutes; // TODO: whats a reasonable number :thinking:
-
-  constructor() {
-    lastSeenTimestamp = block.timestamp;
-  }
-
-  /// @notice Handles an expirable (soft) nonce
-  /// Continue execution if the calldata nonce is equal to the current nonce
-  /// Continue execution if the calldata nonce is one greater AND the timestamp
-  modifier handleSoftNonce(uint256 _nonce) {
-    if (_nonce != nonce) {
-      if (lastSeenTimestamp + EXPIRATION < block.timestamp) {
-        // reset the nonce if it has expired
-        nonce = _nonce;
-      } else {
-        revert InvalidNonce();
-      }
-    }
-
-    lastSeenTimestamp = block.timestamp;
-    unchecked {
-      ++nonce;
-    }
-    _;
-  }
-}
