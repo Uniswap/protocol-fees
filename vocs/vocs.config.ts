4287c0b402c4d951fda973cc324edeeeb54dd988
saucepoint <saucepoint@protonmail.com>
2025-09-18 16:51:53 -0400

    scaffolding

--- vocs/vocs.config.ts
+++ vocs/vocs.config.ts
@@ -6,12 +6,31 @@ export default defineConfig({
   title: 'Docs',
   sidebar: [
     {
+      text: 'Overview',
+      link: '/overview',
+    },
+    {
       text: 'Getting Started',
       link: '/getting-started',
     },
     {
-      text: 'Example',
-      link: '/example',
+      text: 'System Permissions',
+      link: '/system-permissions',
+    },
+    {
+      text: "Guides",
+      collapsed: false,
+      items: [
+        ...fs.readdirSync(path.resolve(process.cwd(), 'docs/pages/guides'))
+          .filter((f) => f.endsWith(".mdx"))
+          .map((file) => {
+            const key = path.basename(file, ".mdx");
+            return {
+              text: key.split("-").map((w) => w.charAt(0).toUpperCase() + w.slice(1)).join(" "),
+              link: `/guides/${key}`,
+            };
+          }),
+      ],
     },
     {
       text: "Technical Reference",
