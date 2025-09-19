5b51dc9e1c507f39040d98cadf2878baeb6ae80f
Sara Reynolds <snreynolds2506@gmail.com>
2025-09-19 01:14:56 +0100

    feat: introducing uni vesting

---
+++ src/UniVesting.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+import {IUNI} from "./interfaces/IUNI.sol";
+import {SafeCast} from "openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
+import {VestingLib} from "./libraries/VestingLib.sol";
+
+/// TODO: Add Ownable.
+contract UniVesting {
+  using VestingLib for *;
+
+  /// Thrown when the minting timestamp has not been updated, but a vesting window is being started.
+  error MintingWindowClosed();
+
+  /// Thrown if trying to intiate a vesting window with no balance.
+  error NothingToVest();
+
+  /// Thrown if the vesting window is not complete.
+  error ActiveVestingWindow();
+
+  /// The UNI token contract.
+  IUNI public immutable UNI;
+
+  /// The duration of each period, ie. 30 days.
+  uint256 public immutable periodDuration;
+
+  /// The total vesting period, ie. 365 days.
+  uint256 public immutable totalVestingPeriod;
+
+  /// The total number of periods in the vesting window, ie 12.
+  uint256 public immutable totalPeriods;
+
+  /// The checkpoint of the minting allowed after timestamp set on the UNI token contract. Stored to
+  /// keep track of minting windows.
+  /// Vesting should not be allowed to start if the minting window has not changed.
+  uint256 public mintingAllowedAfterCheckpoint;
+
+  /// The amount of tokens that are being vested in this window.
+  uint256 public amountVesting;
+
+  /// The start time of the vesting window.
+  uint256 public startTime;
+
+  /// If positive, it's the amount of tokens that have been claimed in this vesting window. It will
+  /// be negative if there are tokens leftover from previous vesting windows, and a NEW vesting
+  /// window has begun.
+  int256 public claimed;
+
+  constructor(address _uni, uint256 _periodDuration, uint256 _totalVestingPeriod) {
+    UNI = IUNI(_uni);
+    periodDuration = _periodDuration;
+    totalVestingPeriod = _totalVestingPeriod;
+    totalPeriods = _totalVestingPeriod / _periodDuration;
+    mintingAllowedAfterCheckpoint = UNI.mintingAllowedAfter();
+  }
+
+  function start() public {
+    require(UNI.mintingAllowedAfter() > mintingAllowedAfterCheckpoint, MintingWindowClosed());
+    require(totalVested() == amountVesting, ActiveVestingWindow());
+
+    /// Calculate the amount to vest.
+    uint256 balance = UNI.balanceOf(address(this));
+    uint256 leftover = amountVesting.sub(claimed);
+    amountVesting = balance - leftover;
+    require(amountVesting > 0, NothingToVest());
+
+    /// Allow the leftover tokens to be claimed.
+    claimed = -SafeCast.toInt256(leftover);
+
+    /// Reset the vesting schedule.
+    startTime = block.timestamp;
+    mintingAllowedAfterCheckpoint = UNI.mintingAllowedAfter();
+  }
+
+  /// TODO: This should be callable with a recipient and only callable by the owner.
+  /// Claim any already vested tokens, updating the claimed amount. It's possible that this sets the
+  /// claimed amount to zero, if the only claimable tokens are leftover from a previous vest.
+  function claim() public {
+    uint256 _claimable = claimable();
+    claimed = claimed.add(_claimable);
+    UNI.transfer(msg.sender, _claimable);
+  }
+
+  /// The total amount of tokens that are claimable. This COULD return a value greater than
+  /// amountVesting if multiple vesting windows have been started and have leftover tokens.
+  function claimable() public view returns (uint256) {
+    return totalVested().sub(claimed);
+  }
+
+  /// The totalVested amount in this vesting window. Bounded by 0 and amountVesting.
+  function totalVested() public view returns (uint256) {
+    if (block.timestamp < startTime) return 0;
+    if (block.timestamp >= startTime + totalVestingPeriod) return amountVesting;
+
+    uint256 elapsed = block.timestamp - startTime;
+
+    return (elapsed / periodDuration) * amountVesting / totalPeriods;
+  }
+}
---
+++ test/Vesting.t.sol
@@ -0,0 +1,152 @@
+pragma solidity ^0.8.29;
+
+import {Test} from "forge-std/Test.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+import {SafeCast} from "openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
+import {UniVesting} from "../src/UniVesting.sol";
+import {IUNI} from "../src/interfaces/IUNI.sol";
+
+import {console2} from "forge-std/console2.sol";
+
+contract VestingTest is Test {
+  MockERC20 public vestingToken;
+  UniVesting public vesting;
+
+  uint256 public constant INITIAL_TOKEN_AMOUNT = 1000e18;
+
+  function setUp() public {
+    vestingToken = new MockERC20("VestingToken", "VTK", 18);
+
+    vm.mockCall(
+      address(vestingToken),
+      abi.encodeWithSelector(IUNI.mintingAllowedAfter.selector),
+      abi.encode(block.timestamp - 1)
+    );
+
+    vesting = new UniVesting(address(vestingToken), 30 days, 365 days);
+  }
+
+  function test_start_reverts_whenMintingWindowNotChanged() public {
+    vm.expectRevert(UniVesting.MintingWindowClosed.selector);
+    vesting.start();
+  }
+
+  function test_start() public {
+    _mockMintAt(block.timestamp, INITIAL_TOKEN_AMOUNT);
+    assertEq(vestingToken.balanceOf(address(vesting)), INITIAL_TOKEN_AMOUNT);
+
+    vesting.start();
+
+    assertEq(vesting.claimed(), 0);
+    assertEq(vesting.amountVesting(), INITIAL_TOKEN_AMOUNT);
+    assertEq(vesting.startTime(), block.timestamp);
+    assertEq(vesting.mintingAllowedAfterCheckpoint(), block.timestamp);
+    assertEq(vesting.totalVested(), 0);
+    assertEq(vesting.claimable(), 0);
+  }
+
+  function test_claim() public {
+    _mockMintAt(block.timestamp, INITIAL_TOKEN_AMOUNT);
+
+    vesting.start();
+    assertEq(vesting.totalVested(), 0);
+    assertEq(vesting.claimed(), 0);
+    assertEq(vesting.claimable(), 0);
+
+    /// Even after an attempt to claim no state is changed.
+    vesting.claim();
+    assertEq(vesting.totalVested(), 0);
+    assertEq(vesting.claimed(), 0);
+    assertEq(vesting.claimable(), 0);
+    assertEq(vestingToken.balanceOf(address(vesting)), INITIAL_TOKEN_AMOUNT);
+  }
+
+  function test_claim_after_1_period() public {
+    _mockMintAt(block.timestamp, INITIAL_TOKEN_AMOUNT);
+
+    vesting.start();
+    vm.warp(block.timestamp + 30 days);
+    /// Total vested should be 1/numPeriods of the inital amount (ie 1/12)
+    uint256 expectedTotalVested = INITIAL_TOKEN_AMOUNT / vesting.totalPeriods();
+    assertEq(vesting.totalVested(), expectedTotalVested);
+    assertEq(vesting.claimed(), 0);
+    assertEq(vesting.claimable(), expectedTotalVested);
+
+    vesting.claim();
+    assertEq(vesting.totalVested(), expectedTotalVested);
+    assertEq(vesting.claimed(), SafeCast.toInt256(expectedTotalVested));
+    assertEq(vesting.claimable(), 0);
+  }
+
+  function test_start_with_leftover() public {
+    _mockMintAt(block.timestamp, INITIAL_TOKEN_AMOUNT);
+
+    vesting.start();
+    uint256 newTime = block.timestamp + vesting.totalVestingPeriod();
+    vm.warp(newTime);
+    /// Total vested should be the full amount.
+    uint256 expectedTotalVested = INITIAL_TOKEN_AMOUNT;
+    assertEq(vesting.totalVested(), expectedTotalVested);
+    assertEq(vesting.claimed(), 0);
+    assertEq(vesting.claimable(), expectedTotalVested);
+
+    /// Without calling claim, we should be able to start a new vest (assuming a new mint has
+    /// happened).
+    _mockMintAt(newTime, INITIAL_TOKEN_AMOUNT * 2);
+    vesting.start();
+
+    /// The leftover tokens from the previous vest are cached in the claimed amount.
+    assertEq(vesting.claimed(), -SafeCast.toInt256(INITIAL_TOKEN_AMOUNT));
+    assertEq(vesting.amountVesting(), INITIAL_TOKEN_AMOUNT * 2);
+    assertEq(vesting.startTime(), newTime);
+    assertEq(vesting.mintingAllowedAfterCheckpoint(), newTime);
+    assertEq(vesting.totalVested(), 0);
+    assertEq(vesting.claimable(), INITIAL_TOKEN_AMOUNT);
+  }
+
+  function test_claim_with_leftover() public {
+    /// Start first vesting window.addmod
+    _mockMintAt(block.timestamp, INITIAL_TOKEN_AMOUNT);
+    vesting.start();
+
+    /// Warp to the end of the first vesting window, without a claim. The total inital amount will
+    /// be vested.
+    uint256 newTime = block.timestamp + vesting.totalVestingPeriod();
+    vm.warp(newTime);
+    uint256 expectedTotalVested = INITIAL_TOKEN_AMOUNT;
+    assertEq(vesting.totalVested(), expectedTotalVested);
+    assertEq(vesting.claimed(), 0);
+    assertEq(vesting.claimable(), expectedTotalVested);
+    assertEq(vesting.amountVesting(), INITIAL_TOKEN_AMOUNT);
+
+    /// Without calling claim, we should be able to start a new vest (assuming a new mint has
+    /// happened).
+    _mockMintAt(newTime, INITIAL_TOKEN_AMOUNT * 2);
+    vesting.start();
+
+    assertEq(vesting.totalVested(), 0);
+    assertEq(vesting.claimable(), INITIAL_TOKEN_AMOUNT);
+    assertEq(vesting.amountVesting(), INITIAL_TOKEN_AMOUNT * 2);
+    assertEq(vesting.claimed(), -SafeCast.toInt256(INITIAL_TOKEN_AMOUNT));
+    /// This is the leftover amount from the previous vest.
+
+    /// Calling claim should only take the leftover tokens from the previous vest.
+    vesting.claim();
+    assertEq(vesting.claimable(), 0);
+    /// The claimed amount is 0 because the claim call only took tokens from the previous vest.
+    assertEq(vesting.claimed(), 0);
+    assertEq(vesting.amountVesting(), INITIAL_TOKEN_AMOUNT * 2);
+    assertEq(vesting.totalVested(), 0);
+  }
+
+  function _mockMintAt(uint256 timestamp, uint256 mintAmount) public {
+    /// Update the mintingAllowedAfter timestamp.
+    vm.mockCall(
+      address(vestingToken),
+      abi.encodeWithSelector(IUNI.mintingAllowedAfter.selector),
+      abi.encode(timestamp)
+    );
+
+    vestingToken.mint(address(vesting), mintAmount);
+  }
+}
